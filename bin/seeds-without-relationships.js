f49a3da51d96850c49fc5aa62aa72bae98280d49
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-03-21 14:09:50 +0100

    add seed files with Books + Authors

---
+++ bin/seeds-without-relationships.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+const Book = require('../models/Book.model');
+const Author = require('../models/Author.model');
+
+
+
+const books = [
+    {
+        title: "The Hunger Games",
+        description:
+            "The Hunger Games is a 2008 dystopian novel by the American writer Suzanne Collins. It is written in the voice of 16-year-old Katniss Everdeen, who lives in the future, post-apocalyptic nation of Panem in North America. The Capitol, a highly advanced metropolis, exercises political control over the rest of the nation. The Hunger Games is an annual event in which one boy and one girl aged 12–18 from each of the twelve districts surrounding the Capitol are selected by lottery to compete in a televised battle royale to the death.",
+        rating: 10
+    },
+    {
+        title: "Harry Potter v.1 (Harry Potter and the Philosopher's Stone)",
+        description:
+            "Harry Potter v1",
+        rating: 9
+    },
+    {
+        title: "Harry Potter v.2 (The Chamber Of Secrets)",
+        description:
+            "Harry Potter v2",
+        rating: 9
+    }
+];
+
+
+const authors = [
+    {
+        name: "Suzanne Collins",
+        age: 40,
+        country: "UK"
+    },
+    {
+        name: "J.K. Rowling",
+        age: 50,
+        country: "UK"
+    }
+];
+
+
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/library-project';
+
+mongoose
+    .connect(MONGO_URI)
+    .then((x) => {
+        console.log(
+            `Connected to Mongo! Database name: "${x.connections[0].name}"`
+        );
+
+        //return Book.deleteMany({}); //WARNING: this will delete all books in your DB !!
+    })
+    .then( (response) => {
+        console.log(response);
+        
+        //return Author.deleteMany({}); //WARNING: this will delete all authors in your DB !!
+    })
+    .then( (response) => {
+        console.log(response);
+        
+        const booksPromise = Book.create(books);
+        const authorsPromise = Author.create(authors);
+
+        return Promise.all([booksPromise, authorsPromise]);
+    })
+    .then( result => {
+        const booksCreated = result[0];
+        const authorsCreated = result[1];
+        console.log(`Number of books created... ${booksCreated.length} `);
+        console.log(`Number of authors created... ${authorsCreated.length} `);
+
+        // Once created, close the DB connection
+        mongoose.connection.close();
+
+    })
+    .catch(e => console.log("error seeding data in DB....", e));
