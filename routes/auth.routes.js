a99a41b85f64a4eb7b57e5a34d7ab2ed15618b3b
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-03-22 15:32:31 +0100

    (auth) implement logout

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -99,6 +99,16 @@ router.post('/login', (req, res, next) => {
 
 
 
+//POST /logout
+router.post('/logout', (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) next(err);
+    res.redirect('/');
+  });
+});
+
+
+
 //GET user-profile
 router.get('/user-profile', (req, res) => {
   res.render('auth/user-profile', {user: req.session.currentUser})
