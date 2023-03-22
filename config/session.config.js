ef163cf3244d68b55c300ab58a1a550918275931
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-03-22 15:04:23 +0100

    (auth) store session data in DB with connect-mongo

--- config/session.config.js
+++ config/session.config.js
@@ -1,4 +1,5 @@
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 // since we are going to USE this middleware in the app.js,
 // let's export it and have it receive a parameter
@@ -21,7 +22,11 @@ module.exports = app => {
                 secure: process.env.NODE_ENV === 'production',
                 httpOnly: true,
                 maxAge: 1000 * 60 * 60 * 24 // 24h
-            }
+            },
+            store: MongoStore.create({
+                mongoUrl: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/library-project",
+                ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day
+            })
         })
     );
 };
