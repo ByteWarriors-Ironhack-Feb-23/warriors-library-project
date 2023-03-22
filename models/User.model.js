a0fbe6e93260f2356e854d6bd164ea7685d7cd3b
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-03-22 10:41:16 +0100

    update User model (email only)

--- models/User.model.js
+++ models/User.model.js
@@ -1,29 +1,22 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the User model to whatever makes sense in this case
+
 const userSchema = new Schema(
   {
-    username: {
-      type: String,
-      trim: true,
-      required: false,
-      unique: true
-    },
     email: {
       type: String,
-      required: true,
+      required: [true, "Email is required"],
       unique: true,
       lowercase: true,
       trim: true
     },
-    password: {
+    passwordHash: {
       type: String,
-      required: true
-    }
+      required: [true, "Password required"]
+    },
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`    
-    timestamps: true
+    timestamps: true,
   }
 );
 
